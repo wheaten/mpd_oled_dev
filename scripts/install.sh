5501c49a08f0fc2dd062bb235c6b7f11ec18c0b0
wheaten <wheaten@hotmail.nl>
2022-12-04 12:37:54 +0100

    Add files via upload

---
+++ scripts/install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+cd ~
+echo "-----------------------------------------------------------------------"
+echo "               Installing needed packages for volumio...               "
+echo "-----------------------------------------------------------------------"
+echo ""
+sudo apt-get update
+sudo apt-get install -y build-essential autoconf make libtool libfftw3-dev libiniparser-dev libmpdclient-dev libi2c-dev lm-sensors libasound2-dev autoconf-archive i2c-tools dkms
+
+cd ~
+echo "-----------------------------------------------------------------------"
+echo "               Downloading kernel-headers for volumio...               "
+echo "-----------------------------------------------------------------------"
+echo ""
+if [ ! -d "/usr/src/linux-headers-5.10.139-volumio" ] 
+then
+  wget https://github.com/volumio/x86-kernel-headers/raw/master/linux-headers-5.10.139-volumio_5.10.139-volumio-1_amd64.deb 
+  sudo dpkg -i linux-headers-5.10.139-volumio_5.10.139-volumio-1_amd64.deb
+  sudo ln -s /usr/src/linux-headers-5.10.139-volumio /lib/modules/5.10.139-volumio/build
+fi
+
+cd ~
+echo "-----------------------------------------------------------------------"
+echo "               Installing the i2c-ch341-usb drivers...                 "
+echo "-----------------------------------------------------------------------"
+echo ""
+git clone https://github.com/gschorcht/i2c-ch341-usb.git
+cd i2c-ch341-usb
+make && sudo make install >> "/dev/null"
+cd ~
+
+echo "-----------------------------------------------------------------------"
+echo "               Adding Volumio to the i2c group...                      " 
+echo "-----------------------------------------------------------------------"
+echo ""
+if id -Gn "volumio"|grep -c "i2c"; then
+    echo "volumio already belongs to group i2c"
+else
+    sudo addgroup volumio i2c
+    #newgrp - i2c
+fi
+
+echo "-----------------------------------------------------------------------"
+echo "               Installing the repo for CAVA...                         "
+echo "-----------------------------------------------------------------------"
+echo ""
+git clone https://github.com/karlstav/cava
+cd cava
+./autogen.sh && ./configure --disable-input-portaudio --disable-input-sndio --disable-output-ncurses --disable-input-pulse --program-prefix=mpd_oled_ && make  && sudo make install-strip >> "/dev/null"
+cd ~
+
+echo "-----------------------------------------------------------------------"
+echo "               Installing and compiling the libu8g2 library.           "
+echo "                  This will take some time. Hold on...                 "
+echo "-----------------------------------------------------------------------"
+echo ""
+git clone https://github.com/antiprism/libu8g2arm.git
+cd libu8g2arm
+./bootstrap
+mkdir build && cd build
+CPPFLAGS="-W -Wall -Wno-psabi" ../configure --prefix=/usr/local  && make 
+cd ~
+
+ echo "-----------------------------------------------------------------------"
+ echo "               Installing the DEV branch of MPD_OLED...                "
+ echo "-----------------------------------------------------------------------"
+ echo ""
+ git clone https://github.com/wheaten/mpd_oled_dev
+ cd mpd_oled_dev
+ ./bootstrap 
+ LIBU8G2_DIR=../libu8g2arm CPPFLAGS="-W -Wall -Wno-psabi" ./configure --prefix=/usr/local &&  make  >> "/dev/null" &&  sudo make install-strip >> "/dev/null"
+ 
+ ./bootstrap 
+ LIBU8G2_DIR=../libu8g2arm CPPFLAGS="-W -Wall -Wno-psabi" ./configure --prefix=/usr/local &&  make  >> "/dev/null" &&  sudo make install-strip >> "/dev/null"
+ cd ~
+
+sudo mpd_oled_volumio_mpd_conf_install
+
+
+# #sudo rm -rf /usr/src/linux-headers-5.10.139-volumio
+# #sudo rm -rf /lib/modules/5.10.139-volumio
+
+# #sudo apt-mark auto '^linux-headers-5.10.139-volumio'
+# #sudo apt autoremove
+# #sudo reboot
